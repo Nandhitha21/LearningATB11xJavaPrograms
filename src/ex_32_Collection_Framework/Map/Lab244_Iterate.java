977d4274d27fa56af068d43bb2896f19b5eadcc7
Nand <nandhithap06@gmail.com>
2025-04-20 19:03:14 +0530

    Java code Pushing

---
+++ src/ex_32_Collection_Framework/Map/Lab244_Iterate.java
@@ -0,0 +1,25 @@
+package ex_32_Collection_Framework.Map;
+
+import java.util.*;
+public class Lab244_Iterate {
+    public static void main(String[] args) {
+
+        Map<String,Integer> map = new HashMap();
+        map.put("id", 1);
+        map.put("id2", 2);
+        map.put("id3", 34);
+        map.put("id4", null);
+        map.put("id5", null);
+        map.put(null, 100);
+
+        for(Map.Entry<String,Integer> item: map.entrySet()){
+            System.out.println(item.getKey() + " -> " +item.getValue());
+        }
+
+
+
+
+
+
+    }
+}
